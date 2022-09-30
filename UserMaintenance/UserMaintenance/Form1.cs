f961137efedceec32bea8a32137b6da9431e91e7
Roland Nagy <nagy.roland210@gmail.com>
2022-09-30 18:47:00 +0200

    UserHozzáadás

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,17 +7,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
+
         public Form1()
         {
             InitializeComponent();
+            //design elemek beállítása
             label1.Text = Resource1.Lastname;
             label2.Text = Resource1.Firstname;
             button1.Text = Resource1.Add;
+
+            //listbox 1 beállítása
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+
+            //button1 beállítása
+            button1.Click += Button1_Click;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //másik féle értékadás 
+            //var NewUser = new User()
+            // { 
+            //    LastName = textBox1.Text,
+            //    FirstName = textBox2.Text 
+            // };
+
+            User NewUser = new User();
+            NewUser.LastName = textBox1.Text;
+            NewUser.FirstName = textBox2.Text;
+
+            users.Add(NewUser);
         }
     }
 }
+
+    
+
