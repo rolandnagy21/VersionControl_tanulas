a8c2b8e9a2ebfaa5e7d667a4747f0ff60823d8ba
Roland Nagy <nagy.roland210@gmail.com>
2022-10-08 00:04:19 +0200

    formázás

--- gyak4_WEYEWU/gyak4_WEYEWU/Form1.cs
+++ gyak4_WEYEWU/gyak4_WEYEWU/Form1.cs
@@ -144,6 +144,18 @@ namespace gyak4_WEYEWU
             //.Value2 = $"= 1000000 * xlSheet.get_Range(GetCell(2, adatok.GetLength(1) - 1), GetCell(1 + adatok.GetLength(0), adatok.GetLength(1) - 1)).Value2/xlSheet.get_Range(GetCell(2, adatok.GetLength(1) - 2), GetCell(1 + adatok.GetLength(0), adatok.GetLength(1) - 2)).Value2";
             .Value2 = $"= 1000000 * {H2}:{Hvége}/{G2}:{Gvége}";
             //= 1000000 * H2:H105 / G2:G105
+
+            Excel.Range FejlécTartomány = xlSheet.get_Range(GetCell(1, 1), GetCell(1, fejlécek.Length));
+            FejlécTartomány.Font.Bold = true;
+            FejlécTartomány.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            FejlécTartomány.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            FejlécTartomány.EntireColumn.AutoFit();
+            FejlécTartomány.RowHeight = 40;
+            FejlécTartomány.Interior.Color = Color.LightBlue;
+            FejlécTartomány.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+            Excel.Range egészTáblaTartomány = xlSheet.get_Range(GetCell(1, 1), GetCell(xlSheet.UsedRange.Rows.Count, fejlécek.Length));
+            egészTáblaTartomány.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
         }
 
         private string GetCell(int x, int y)
