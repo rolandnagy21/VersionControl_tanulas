116775a3e5ac33350f254a816f517b32abd0897f
Roland Nagy <nagy.roland210@gmail.com>
2022-10-07 22:25:12 +0200

    1-8Feladatok

--- gyak4_WEYEWU/gyak4_WEYEWU/Form1.cs
+++ gyak4_WEYEWU/gyak4_WEYEWU/Form1.cs
@@ -7,14 +7,142 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace gyak4_WEYEWU
 {
     public partial class Form1 : Form
     {
+        List<Flat> Flats; //Flat típusú elemekből álló listára mutató referencia (Nem kell inicializálni new-val)
+        RealEstateEntities1 context = new RealEstateEntities1();
+
+        //üres változók
+        Excel.Application xlApp;
+        // Az MS Excel alkalmazás
+        Excel.Workbook xlWB;
+        // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet;
+        // Munkalap a munkafüzeten belül
+
         public Form1()
         {
             InitializeComponent();
+
+            LoadData();
+            CreateExcel();
         }
+
+        void LoadData()
+        {
+            Flats = context.Flats.ToList();
+        }
+
+        void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();   
+                // Excel elindítása és az applikáció objektum betöltése
+
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                // Új munkafüzet létrehozása
+
+                xlSheet = xlWB.ActiveSheet;
+                // Új munkalap létrehozása
+
+                CreateTable();
+                // Tábla létrehozása
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+                // Control átadása a felhasználónak
+            }
+
+            catch (Exception ex) // Hibakezelés a beépített hibaüzenettel
+            {
+                string errMsg = string.Format("Hiba: {0}\nEbben a sorban: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Hibaüzenet"); //elnevezett MessageBox
+
+                xlWB.Close(false, Type.Missing, Type.Missing); //alapértelmezett értékekkel hívjuk meg így a Close-t szerintem
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+                //a catch ágban a munkafüzetből kilépés, és az Excel Applikáció bezárása, és az ezeknek megfelelő változók üressé tétele
+            }
+        }
+
+        void CreateTable()
+        {
+            string[] fejlécek = new string[]
+            {
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m2)",
+                "Ár (mFt)",
+                "Négyzetméter ár (Ft/m2)"
+            };
+
+            for (int i = 0; i < fejlécek.Length; i++)
+            {
+                xlSheet.Cells[1, (i + 1)] = fejlécek[i];
+            }
+
+            object[,] adatok = new object[Flats.Count(), fejlécek.Length];
+
+
+            int sor = 0;
+            foreach (Flat lakás in Flats)
+            {
+                adatok[sor, 0] = lakás.Code;
+                adatok[sor, 1] = lakás.Vendor;
+                adatok[sor, 2] = lakás.Side;
+                adatok[sor, 3] = lakás.District;
+                if (lakás.Elevator)
+                {
+                    adatok[sor, 4] = "Van";
+                }
+                else
+                {
+                    adatok[sor, 4] = "Nincs";
+
+                }
+                adatok[sor, 5] = lakás.NumberOfRooms;
+                adatok[sor, 6] = lakás.FloorArea;
+                adatok[sor, 7] = lakás.Price;
+                adatok[sor, 8] = "";
+
+                sor++;
+            }
+
+            xlSheet.get_Range(
+              GetCell(2, 1), //GetCell(2,1)
+              GetCell(1 + adatok.GetLength(0), adatok.GetLength(1))) //GetCell(1 + 104, 9)
+              .Value2 = adatok;
+        }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y; //1, 9
+            int modulo;
+
+            while (dividend > 0)
+            {
+                //kommentben: GetCell(2,1), GetCell(1 + 104, 9)
+
+                modulo = (dividend - 1) % 26; //0, 8
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate; //"A", "I"
+                //Convert.ToChar(65) = A, innentől kezdődnek a betűk (ASCII tábla)
+                dividend = (int)((dividend - modulo) / 26); // 0, 0
+            }
+            ExcelCoordinate += x.ToString(); //"A2", "I105"
+
+            return ExcelCoordinate;
+        }
+
     }
 }
