17696b890dfa604926257d018944d67ea2375af9
Roland Nagy <nagy.roland210@gmail.com>
2022-11-04 20:32:24 +0100

    Absztrakció felhasználása a gyárakban

--- gyak6_WEYEWU/gyak6_WEYEWU/Entities/CarFactory.cs
+++ gyak6_WEYEWU/gyak6_WEYEWU/Entities/CarFactory.cs
@@ -7,11 +7,11 @@ using System.Threading.Tasks;
 
 namespace gyak6_WEYEWU.Entities
 {
-    public class BallFactory : IToyFactory
+    public class CarFactory : IToyFactory
     {
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
     }
 }
