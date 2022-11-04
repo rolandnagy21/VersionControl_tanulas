9e351ace2991e3cf361711c8549721f4572a9209
Roland Nagy <nagy.roland210@gmail.com>
2022-11-04 19:53:02 +0100

    labda létrehozása

---
+++ gyak6_WEYEWU/gyak6_WEYEWU/Entities/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace gyak6_WEYEWU.Entities
+{
+    internal class Ball : Label
+    {
+        public Ball()
+        {
+            AutoSize = false;
+            Width = 50;
+            Paint += Ball_Paint;
+            Height = Width;
+            
+        }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+
+        public void MoveBall()
+        {
+            Left = Left + 1;
+        }
+    }
+}
