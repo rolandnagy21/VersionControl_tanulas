18c7beb93434de6638a27adb9e516589195f45d2
Roland Nagy <nagy.roland210@gmail.com>
2022-11-04 20:11:11 +0100

    Absztrakció felhasználása a labdában

--- gyak6_WEYEWU/gyak6_WEYEWU/Entities/Ball.cs
+++ gyak6_WEYEWU/gyak6_WEYEWU/Entities/Ball.cs
@@ -5,33 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
+using gyak6_WEYEWU.Abstractions;
 
 namespace gyak6_WEYEWU.Entities
 {
-    internal class Ball : Label
+    internal class Ball : Toy
     {
-        public Ball()
-        {
-            AutoSize = false;
-            Width = 50;
-            Paint += Ball_Paint;
-            Height = Width;
-            
-        }
-
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            DrawImage(e.Graphics);
-        }
-
-        protected void DrawImage(Graphics g)
+        protected override void DrawImage(Graphics g)
         {
             g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
-        }
-
-        public void MoveBall()
-        {
-            Left = Left + 1;
         }
     }
 }
