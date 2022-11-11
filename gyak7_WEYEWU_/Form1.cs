032d5958868d3e02eec8f60bec92235b227e6c74
Roland Nagy <nagy.roland210@gmail.com>
2022-11-11 23:08:27 +0100

    4 Adatok betöltése

--- gyak7_WEYEWU_/Form1.cs
+++ gyak7_WEYEWU_/Form1.cs
@@ -12,9 +12,17 @@ namespace gyak7_WEYEWU_
 {
     public partial class Form1 : Form
     {
+        List<Tick> Ticks;
+
+        PortfolioEntities context = new PortfolioEntities();
+
         public Form1()
         {
             InitializeComponent();
+           
+            Ticks = context.Tick.ToList();
+
+            dataGridView1.DataSource = Ticks;
         }
     }
 }
