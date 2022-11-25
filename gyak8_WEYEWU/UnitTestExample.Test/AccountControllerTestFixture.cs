50df482b0d43807e8780fd8b8b0e3d057c7599e0
Roland Nagy <nagy.roland210@gmail.com>
2022-11-25 23:56:01 +0100

    Alosztály hívás kezelése

--- gyak8_WEYEWU/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ gyak8_WEYEWU/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -1,11 +1,14 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using System;
 using System.Activities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTestExample.Abstractions;
 using UnitTestExample.Controllers;
+using UnitTestExample.Entities;
 
 namespace UnitTestExample.Test
 {
@@ -31,35 +34,6 @@ namespace UnitTestExample.Test
             Assert.AreEqual(expectedResult, actualResult);
         }
 
-        [
-        Test,
-        TestCase("irf@uni-corvinus.hu", "Abcd1234"),
-        TestCase("irf@uni-corvinus.hu", "Abcd1234567"),
-        ]
-
-        public void TestRegisterHappyPath(string email, string password)
-        {
-            // Arrange
-            var accountController = new AccountController();
-
-            // Act
-            var actualResult = accountController.Register(email, password);
-
-            // Assert
-            Assert.AreEqual(email, actualResult.Email);
-            Assert.AreEqual(password, actualResult.Password);
-            Assert.AreNotEqual(Guid.Empty, actualResult.ID);
-        }
-
-        [
-        Test,
-        TestCase("irf@uni-corvinus", "Abcd1234"),
-        TestCase("irf.uni-corvinus.hu", "Abcd1234"),
-        TestCase("irf@uni-corvinus.hu", "abcd1234"),
-        TestCase("irf@uni-corvinus.hu", "ABCD1234"),
-        TestCase("irf@uni-corvinus.hu", "abcdABCD"),
-        TestCase("irf@uni-corvinus.hu", "Ab1234"),
-]
         public void TestValidatePassword(string password, bool expectedResult)
         {
             //Arrange
@@ -71,6 +45,43 @@ namespace UnitTestExample.Test
             // Assert
             Assert.AreEqual(expectedResult, actualResult);
         }
+
+        [
+        Test,
+        TestCase("irf@uni-corvinus.hu", "Abcd1234"),
+        TestCase("irf@uni-corvinus.hu", "Abcd1234567"),
+        ]
+
+        public void TestRegisterHappyPath(string email, string password)
+        {
+            // Arrange
+            var accountServiceMock = new Mock<IAccountManager>(MockBehavior.Strict);
+            accountServiceMock
+                .Setup(m => m.CreateAccount(It.IsAny<Account>()))
+                .Returns<Account>(a => a);
+            var accountController = new AccountController();
+            accountController.AccountManager = accountServiceMock.Object;
+
+            // Act
+            var actualResult = accountController.Register(email, password);
+
+            // Assert
+            Assert.AreEqual(email, actualResult.Email);
+            Assert.AreEqual(password, actualResult.Password);
+            Assert.AreNotEqual(Guid.Empty, actualResult.ID);
+            accountServiceMock.Verify(m => m.CreateAccount(actualResult), Times.Once);
+        }
+
+        [
+        Test,
+        TestCase("irf@uni-corvinus", "Abcd1234"),
+        TestCase("irf.uni-corvinus.hu", "Abcd1234"),
+        TestCase("irf@uni-corvinus.hu", "abcd1234"),
+        TestCase("irf@uni-corvinus.hu", "ABCD1234"),
+        TestCase("irf@uni-corvinus.hu", "abcdABCD"),
+        TestCase("irf@uni-corvinus.hu", "Ab1234"),
+]
+
 
         public void TestRegisterValidateException(string email, string password)
         {
@@ -86,6 +97,33 @@ namespace UnitTestExample.Test
             catch (Exception ex)
             {
                 Assert.IsInstanceOf<ValidationException>(ex);
+            }
+        }
+        
+        [
+        Test,
+        TestCase("irf@uni-corvinus.hu", "Abcd1234")
+        ]
+        
+        public void TestRegisterApplicationException(string newEmail, string newPassword)
+        {
+            // Arrange
+            var accountServiceMock = new Mock<IAccountManager>(MockBehavior.Strict);
+            accountServiceMock
+                .Setup(m => m.CreateAccount(It.IsAny<Account>()))
+                .Throws<ApplicationException>();
+            var accountController = new AccountController();
+            accountController.AccountManager = accountServiceMock.Object;
+
+            // Act
+            try
+            {
+                var actualResult = accountController.Register(newEmail, newPassword);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOf<ApplicationException>(ex);
             }
 
             // Assert
